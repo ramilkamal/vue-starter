91b1d6410410c73aedabaddd72948f387aa3ef9d
ramildeveloper <ramilprogrammer@gmail.com>
2023-06-23 09:07:17 +0500

    vue.js counter

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Counter from './components/counter.vue';
+</script>
+
+<template>
+    <Counter msg="Counter" />
+</template>
+
+<style scoped>
+/** styles css */
+</style>
